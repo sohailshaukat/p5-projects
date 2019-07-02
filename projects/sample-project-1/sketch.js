e28d0fd46f2b7e4b7f3e7b0e94631fdfe3b199e2
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-07-03 02:02:22 +0530

    p5 proj displaying p5 added XD

--- projects/sample-project-1/sketch.js
+++ projects/sample-project-1/sketch.js
@@ -7,6 +7,33 @@ function setup() {
 function draw() {
   // put drawing code here
   background(200,0,0);
-  circle(50,50,20);
+  var i;
+  for(i=50;i<150;i+=10){
+  circle(i,50,20);
+  }
+  for(i=50;i<300;i+=10){
+  circle(50,i,20);
+  }
+  for(i=50;i<150;i+=10){
+  circle(150,i,20);
+  }
+  for(i=50;i<150;i+=10){
+  circle(i,150,20);
+  }
+  for(i=100;i<175;i+=10){
+  circle(i,200,20);
+  }
+  for(i=200;i<270;i+=10){
+  circle(100,i,20);
+  }
+  for(i=100;i<175;i+=10){
+  circle(i,270,20);
+  }
+  for(i=270;i<340;i+=10){
+  circle(175,i,20);
+  }
+  for(i=100;i<175;i+=10){
+  circle(i,340,20);
+  }
   console.log(frameCount);
 }
